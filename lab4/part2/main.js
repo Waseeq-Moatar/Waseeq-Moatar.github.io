97d0ef68b2087e456bbd984b793081cff6f1251f
Waseeq-Moatar <waseeqmoatar@gmail.com>
2026-04-10 23:21:57 -0400

    no index.js
    remove index.js

---
+++ lab4/part2/main.js
@@ -0,0 +1,11 @@
+
+/*Name: Waseeq Moatar
+File: filename.html"
+Date: 27 March 2026"
+allowes urser to input info.*/
+
+const displayedImage = document.querySelector(".displayed-img");
+const thumbBar = document.querySelector(".thumb-bar");
+
+const btn = document.querySelector("button");
+const overlay = document.querySelector(".overlay");
