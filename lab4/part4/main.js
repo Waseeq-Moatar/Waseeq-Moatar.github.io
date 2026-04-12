876408183b7942a2d136fa5c6cb471042b6961b2
Waseeq-Moatar <waseeqmoatar@gmail.com>
2026-04-12 19:02:42 -0400

    Update main.js

--- lab4/part4/main.js
+++ lab4/part4/main.js
@@ -33,10 +33,7 @@ class Ball {
 
     
     constructor(x, y, velX, velY, color, size){
-    this.x = x;
-    this.y = y;
-    this.velX = velX;
-    this.velY = velY;
+   super(x, y, velX, velY);
     this.color = color;
     this.size = size;
     }
