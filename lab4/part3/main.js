8e89b79eafb292cf920cbf3cfa1f2991aedfaae4
Waseeq-Moatar <waseeqmoatar@gmail.com>
2026-04-11 21:06:50 -0400

    start par 3

---
+++ lab4/part3/main.js
@@ -0,0 +1,36 @@
+
+/*Name: Waseeq Moatar
+File: filename.html"
+Date: 27 March 2026"
+allowes urser to input info.*/
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+
+const width = (canvas.width = window.innerWidth);
+const height = (canvas.height = window.innerHeight);
+
+function random(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function randomRGB() {
+  return `rgb(${random(0, 255)} ${random(0, 255)} ${random(0, 255)})`;
+}
+
+class Ball {
+    draw () {
+        ctx.beginPath();
+        ctx.fillStyle = this.color;
+        ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI)
+        ctx.fill();
+    }
+    constructor(x, y, velX, velY, color, size){
+    this.x = x;
+    this.y = y;
+    this.velX = velX;
+    this.velY = velY;
+    this.color = color;
+    this.size = size;
+}
+
+}
