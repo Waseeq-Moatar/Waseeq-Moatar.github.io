6eaee686e4c135af1abc5e83234cf1e0b1af42fb
Waseeq-Moatar <waseeqmoatar@gmail.com>
2026-04-12 19:09:17 -0400

    Update main.js

--- lab4/part4/main.js
+++ lab4/part4/main.js
@@ -2,7 +2,7 @@
 /*Name: Waseeq Moatar
 File: filename.html"
 Date: 27 March 2026"
-allowes urser to input info.*/
+user can now eat balls.*/
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 
@@ -24,11 +24,12 @@ class Shape {
     this.y = y;
     this.velX = velX;
     this.velY = velY;
+    this.exists = true;
   }
 }
 
 
-class Ball {
+class Ball extends Shape{
 
 
     
